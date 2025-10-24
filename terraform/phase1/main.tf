3744f8d4dfc89f3dbbc5520cb85d62bbff0ffd0d
derick chama <derickchama299@gmail.com>
2025-10-23 21:00:14 -0400

    newfixss

--- terraform/phase1/main.tf
+++ terraform/phase1/main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-  region = "us-east-1"  # change to your region
+  region = "us-east-1" # Change to your desired AWS region
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345"  # must be globally unique
-}
+  bucket = "my-unique-bucket-name-12345" # Must be globally unique
 
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
